e3eabb1485c6920345648f608a4b8d5151a4f111
aniket <yaniketever@gmail.com>
2023-08-03 12:38:24 +0530

    first_class

---
+++ rough.js
@@ -0,0 +1,53 @@
+// // Q1) Solve the error in below code
+// // Function statement
+// function x(){
+//     console.log("Elon musk");
+// }
+
+// x();
+// y();
+
+// // Function expression
+// var y = function (){
+//     console.log("function expression");
+// }
+
+
+// // Q2) Solve the error in below code
+// // Function statement
+// function x(){
+//     console.log("Elon musk");
+// }
+
+// x();
+// y();
+
+// // Function expression
+// var y = function abc(){
+//     console.log("function expression");
+// }
+
+
+
+// Q2) Solve the error in below code
+// Function statement
+// function x(){
+//     console.log("Elon musk");
+// }
+
+// x();
+
+
+// // Function expression
+// var y = function abc(param){
+//     console.log("output");
+// }
+
+// y(function () {
+//     console.log("passed as an arguement");
+// });
+
+
+
+
+

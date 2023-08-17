9155e13f1fe424e575db2835b3a0a4367ec19ff2
aniket <yaniketever@gmail.com>
2023-08-17 12:51:07 +0530

    integer_to_array

---
+++ logical_questions/integer_to_array.js
@@ -0,0 +1,10 @@
+// Convert the given Integer into an array format
+
+var num = 123
+
+let myArray = value => Number(value);
+
+let output = Array.from(String(num), myArray);
+
+console.log(output);
+
